8d443094018aec850fc4cf96470703e8a5b62d98
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-05 23:07:43 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,14 +2,17 @@ def options
   puts 'Choose your option from the list'
   puts '1 - List all books'
   puts '2 - List all music albums'
-  puts '3 - List all games'
-  puts '4 - List all genres'
-  puts '5 - List all labels'
-  puts '6 - List all authors'
-  puts '7 - Add a book'
-  puts '8 - Add a music album'
-  puts '9 - Add a game'
-  puts '10 - Exit'
+  puts '3 - List all movies'
+  puts '4 - List all games'
+  puts '5 - List all genres'
+  puts '6 - List all labels'
+  puts '7 - List all authors'
+  puts '8 - List all sources'
+  puts '9 - Add a book'
+  puts '10 - Add a music album'
+  puts '11 - Add a movie'
+  puts '12 - Add a game'
+  puts '13 - Exit'
   puts '-----------------------------'
 end
 
@@ -24,20 +27,26 @@ def menu
     when 2
       puts 'music'
     when 3
-      puts 'games'
+      puts 'movies'
     when 4
-      puts 'genres'
+      puts 'games'
     when 5
-      puts 'labels'
+      puts 'genres'
     when 6
-      puts 'authors'
+      puts 'labels'
     when 7
-      puts 'Add  book'
+      puts 'authors'
     when 8
-      puts 'Add music'
+      puts 'sources'
     when 9
-      puts 'Add game'
+      puts 'Add  book'
     when 10
+      puts 'Add music'
+    when 11
+      puts 'Add movie'
+    when 12
+      puts 'Add game'
+    when 13
       exit
     else
       puts 'Kindly enter a valid choice'
