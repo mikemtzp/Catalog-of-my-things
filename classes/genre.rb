d8277ed04d810c712e6046be2fa6b18ba5e385b8
Daf <dafne.azzolina@gmail.com>
2022-09-06 18:25:08 -0300

    Preserve music data correctly

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,8 +1,11 @@
 require_relative 'item'
 
 class Genre < Item
-  def initialize(name)
-    super(id, publish_date, archived)
+  attr_accessor :name, :publish_date
+  attr_reader :id, :items
+
+  def initialize(name, publish_date, archived: false)
+    super(publish_date)
     @name = name
     @id = Random.rand(1..1000)
     @publish_date = Date.parse(publish_date)
@@ -10,8 +13,18 @@ class Genre < Item
     @items = []
   end
 
+  def genre_hash
+    { name: @name, publish_date: @publish_date }
+  end
+
   def add_item(item)
     @items.push(item)
     item.genre = self
   end
 end
+
+new_genre = Genre.new('Electronic', '2000-10-02')
+puts new_genre.name
+puts new_genre.publish_date.year
+puts new_genre.archived
+puts new_genre.archived
--- classes/music_album.rb
+++ classes/music_album.rb
@@ -2,7 +2,6 @@ require_relative 'item'
 
 class MusicAlbum < Item
   attr_accessor :title, :artist, :genre, :on_spotify
-  attr_reader :id, :items
 
   def initialize(title, genre, publish_date, on_spotify: false, archived: false)
     super(publish_date)
@@ -17,20 +16,13 @@ class MusicAlbum < Item
     @archived = true if can_be_archived?
   end
 
+  def album_hash
+    { title: @title, genre: @genre, publish_date: @publish_date, on_spotify: @on_spotify }
+  end
+
   private
 
   def can_be_archived?()
     on_spotify && (DateTime.now.year - @publish_date.year) > 10
   end
 end
-
-musicalbum = MusicAlbum.new('Kid A', 'Electronic', '2000-10-02')
-puts musicalbum.title
-puts musicalbum.genre
-puts musicalbum.publish_date.year
-puts musicalbum.on_spotify
-musicalbum.on_spotify = true
-puts musicalbum.on_spotify
-puts musicalbum.archived
-musicalbum.move_to_archive
-puts musicalbum.archived
