0c3b228315409ab0e6b001947afa1ef7067bab47
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-09-06 19:13:02 +0200

    Add game class

---
+++ game.rb
@@ -0,0 +1,22 @@
+require_relative 'item'
+require 'date'
+
+class Game < Item
+  attr_accessor :multiplayer, :published_date
+  attr_reader :last_played_at
+
+  def initialize(author, publish_date, multiplayer)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = DateTime.now.year
+    @author = author
+  end
+
+  def can_be_archived?
+    super && (DateTime.now.year - @last_played_at) > 2
+  end
+
+  def game_hash
+    { author: @author, publish_date: @publish_date, multiplayer: @multiplayer }
+  end
+end
