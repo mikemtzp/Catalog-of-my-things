ad2ae40cf8ca084bd79dc3ba7760511dba27b802
Daf <dafne.azzolina@gmail.com>
2022-09-06 17:49:18 -0300

    Add methods to music

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,13 +1,20 @@
 require_relative 'item'
 
 class MusicAlbum < Item
+  attr_accessor :title, :artist, :genre, :on_spotify
+  attr_reader :id, :items
+
   def initialize(title, genre, publish_date, on_spotify: false, archived: false)
     super(publish_date)
     @title = title
     @genre = genre
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @on_spotify = on_spotify
     @archived = archived
+  end
+
+  def move_to_archive()
+    @archived = true if can_be_archived?
   end
 
   private
@@ -16,3 +23,14 @@ class MusicAlbum < Item
     on_spotify && (DateTime.now.year - @publish_date.year) > 10
   end
 end
+
+musicalbum = MusicAlbum.new('Kid A', 'Electronic', '2000-10-02')
+puts musicalbum.title
+puts musicalbum.genre
+puts musicalbum.publish_date.year
+puts musicalbum.on_spotify
+musicalbum.on_spotify = true
+puts musicalbum.on_spotify
+puts musicalbum.archived
+musicalbum.move_to_archive
+puts musicalbum.archived
