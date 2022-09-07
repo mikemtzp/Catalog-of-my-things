f281888d15346d803502fa09a673222fbd6843d0
Daf <dafne.azzolina@gmail.com>
2022-09-07 13:33:14 -0300

    Add music and genre sql tables

--- schema.sql
+++ schema.sql
@@ -24,3 +24,22 @@ CREATE TABLE Book (
   cover_state VARCHAR(7) NOT NULL, 
   FOREIGN KEY (item_id) REFERENCES Item (id), 
 );
+
+--  add all properties and associations from the parent Item class as table columns
+
+CREATE TABLE Music_Albums (
+  item_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  title VARCHAR(50) NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  on_spotify BOOLEAN NOT NULL
+  FOREIGN KEY (item_id) REFERENCES Item (id)
+)
+
+CREATE TABLE Genres (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL
+);
