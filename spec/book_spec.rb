79ddae7bb24257016b393cd5ed9c468a5a79a4bd
mikemtzp <mikemp8226@gmail.com>
2022-09-07 18:51:42 -0500

    Fix books and labels tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require_relative '../classes/book'
 
 describe Book do
   before(:each) do
-    @book = Book.new('Dracula', 'Bram Stoker', '1998-03-04', 'Penguin', 'Good')
+    @book = Book.new('Dracula', '1998-03-04', 'Penguin', 'Good', 'Black')
   end
 
   it 'Returns @book as an instance of Book class' do
@@ -11,10 +11,6 @@ describe Book do
 
   it 'Contains title of the book' do
     expect(@book.title).to eq('Dracula')
-  end
-
-  it 'Contains author of the book' do
-    expect(@book.author).to eq('Bram Stoker')
   end
 
   it 'Contains published date of the book' do
@@ -28,27 +24,31 @@ describe Book do
   it 'Contains cover state of the book' do
     expect(@book.cover_state).to eq('Good')
   end
+
+  it 'Contains label of the book' do
+    expect(@book.label).to eq('Black')
+  end
 end
 
 describe 'Book methods' do
   it 'can_be_archived should return true when publish date is greater than 10 years' do
-    book = Book.new('Dracula', 'Bram Stoker', '1998-03-04', 'Penguin', 'Good')
+    book = Book.new('Dracula', '1998-03-04', 'Penguin', 'Good', 'Black')
     expect(book.send(:can_be_archived?)).to eq(true)
   end
 
   it 'can_be_archived should return true when cover state equals "bad"' do
-    book = Book.new('Dracula', 'Bram Stoker', '2018-03-04', 'Penguin', 'bad')
+    book = Book.new('Dracula', '2018-03-04', 'Penguin', 'bad', 'Black')
     expect(book.send(:can_be_archived?)).to eq(true)
   end
 
   it 'can_be_archived should return true when cover state equals "bad" and publish date is greater than 10 years' do
-    book = Book.new('Dracula', 'Bram Stoker', '1998-03-04', 'Penguin', 'bad')
+    book = Book.new('Dracula', '1998-03-04', 'Penguin', 'bad', 'Black')
     expect(book.send(:can_be_archived?)).to eq(true)
   end
 
   it 'can_be_archived should return false when cover state doesn\'t equal "bad"
   and publish date isnt\' greater than 10 years' do
-    book = Book.new('Dracula', 'Bram Stoker', '2018-03-04', 'Penguin', 'Good')
+    book = Book.new('Dracula', '2018-03-04', 'Penguin', 'Good', 'Black')
     expect(book.send(:can_be_archived?)).to eq(false)
   end
 end
